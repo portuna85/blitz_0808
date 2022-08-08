222c3e549dacef4286f3dab0f1b15010233d164f
portuna85 <portuna85@gmail.com>
2022-08-08 12:52:41 +0900

    jenkins

--- src/main/java/com/blitz/springboot/controller/PostsApiController.java
+++ src/main/java/com/blitz/springboot/controller/PostsApiController.java
@@ -17,26 +17,22 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
-    /* CREATE */
     @PostMapping("/posts")
     public ResponseEntity save(@RequestBody PostsDto.Request dto, @LoginUser UserDto.Response user) {
         return ResponseEntity.ok(postsService.save(dto, user.getNickname()));
     }
 
-    /* READ */
     @GetMapping("/posts/{id}")
     public ResponseEntity read(@PathVariable Long id) {
         return ResponseEntity.ok(postsService.findById(id));
     }
 
-    /* UPDATE */
     @PutMapping("/posts/{id}")
     public ResponseEntity update(@PathVariable Long id, @RequestBody PostsDto.Request dto) {
         postsService.update(id, dto);
         return ResponseEntity.ok(id);
     }
 
-    /* DELETE */
     @DeleteMapping("/posts/{id}")
     public ResponseEntity delete(@PathVariable Long id) {
         postsService.delete(id);

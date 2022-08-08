e7776cbfd31deb1ac1d15793a000c319d272f36c
portuna85 <portuna85@gmail.com>
2022-08-08 21:06:49 +0900

    Update

--- src/main/java/com/blitz/springboot/Application.java
+++ src/main/java/com/blitz/springboot/Application.java
@@ -6,17 +6,9 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-/**
- * 추후 빌드 및 배포를 위해 SpringBootServletInitializer 상속
- */
 @SpringBootApplication
 @EnableJpaAuditing
 public class Application extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
